c12457ef6f34ed4551493fad318f041c659737fc
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-08 00:59:05 +0600

    2nd commit
    2nd commit

---
+++ travel-journal-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+
+    return (
+
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand mx-auto" href="/">
+                    <i class="fas fa-globe-americas"></i>
+                    my travel journal.
+                </a>
+            </div>
+        </nav>
+
+    )
+}
